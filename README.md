3f3a1c2d1ac1b35e4c997c449924c1e8b3811702
zit <yitongzhao@163.com>
2020-12-27 16:28:29 +0800

    update

--- README.md
+++ README.md
@@ -1,10 +1,3 @@
-## TOFIX
-1. npm install package 
-@types/package 不在package.json添加
-2. npm install --ignore-scripts 仍然触发 ./hooks/postinstall
-3. npm install @types/package -D --ignore-scrpts 仍然触发 type-sync scirpts postinstall
-(2,3 无实际影响，有一些干扰信息)
-
 ## yarn 使用
 
 需要在`package.json`中添加`postinstall`
@@ -20,3 +13,7 @@
 1. 完全匹配安装命令（add、install）
 2. 删除包（uninstall、remove）操作处理
 3. 卸载时，删除.hooks/postinstall
+
+## Record
+1. npm install xxx 后执行 ./hooks/postinstall 只能安装一个包
+2. `exec('npm install @types/xxx')` 执行太慢
--- index.js
+++ index.js
@@ -1,20 +1,15 @@
 #!/usr/bin/env node
 
-const https = require("https");
-const gzip = require("zlib").createGunzip();
 const { execSync } = require("child_process");
 const { readFileSync } = require("fs");
+// https://typespublisher.blob.core.windows.net/typespublisher/data/search-index-min.json
 const types_source = require("./search-index-min.json");
 
 // get package name
 // npm_config_argv: '{"remain":[],"cooked":["add"],"original":["add","test","-D"]}',
-// create .hooks/postinstall file
 const { npm_config_argv, npm_execpath } = process.env;
 const { original } = JSON.parse(npm_config_argv);
 const pkg_name = String(original[1] || "").toLowerCase();
-// types search url
-// see https://github.com/microsoft/DefinitelyTyped-tools/tree/d9f558da52e5d9e383cd4ba39dccc239e5d1bc23/packages/publisher#create-a-search-index
-// const types_url = 'https://typespublisher.blob.core.windows.net/typespublisher/data/search-index-min.json';
 
 (function () {
   if (!pkg_name) {
@@ -22,25 +17,27 @@ const pkg_name = String(original[1] || "").toLowerCase();
   }
 
   try {
-    const keyObj = {};
+    const key_obj = {};
     types_source.forEach(
-      (ele) => (keyObj[String(ele.l).toLowerCase()] = ele.t)
+      (ele) => (key_obj[String(ele.l).toLowerCase()] = ele.t)
     );
-    const type_pkg_name = keyObj[pkg_name];
+    const type_pkg_name = key_obj[pkg_name];
 
     if (type_pkg_name) {
       const prefix_name = `@types/${type_pkg_name}`;
-      // if package.json has installed
-      if (ifPkgHasInstalled(prefix_name)) {
+      // is package.json has installed
+      if (isPkgHave(prefix_name)) {
         console.log(`${prefix_name} exists. Nothing is installed.`);
         return;
       }
 
       if (npm_execpath.indexOf("npm") > -1) {
-        console.log(`npm install ${prefix_name} -D`);
-        execSync(`npm install ${prefix_name} -D --ignore-scripts`);
+        // npm has too many problem than i can't solve now
+        // so, ban it temporarily(maybe)
+        // console.log(`npm install ${prefix_name} -D`);
+        // execSync(`npm install ${prefix_name} -D --ignore-scripts`);
       } else {
-        console.log(`yarn add ${prefix_name} -D`);
+        console.log(`executing yarn add ${prefix_name} -D`);
         execSync(`yarn add ${prefix_name} -D`);
       }
       console.log(`${prefix_name} has been installed.`);
@@ -50,8 +47,8 @@ const pkg_name = String(original[1] || "").toLowerCase();
   }
 })();
 
-// if package.json has installed
-function ifPkgHasInstalled(name) {
+// is package.json has installed
+function isPkgHave(name) {
   const file_path = process.argv[1].split("node_modules")[0] + "package.json";
   const pkgStr = readFileSync(file_path, { encoding: "utf-8" });
   const pkgObj = JSON.parse(pkgStr);
